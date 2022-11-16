897a48a86999eb72b98592182ba6b084fd12588a
ElRafo92 <rafael.martinez@outlook.it>
2022-11-16 10:18:44 +0100

    corretto esercizio fatte milestones 1 e 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 /*=====================================
 =======================================
             MAIN VUE JS
@@ -18,13 +17,28 @@ const { createApp } = Vue
             },
         
         tasks: [
+          {
+            text: 'fare la spesa',
+            done: false
+          },
+          {
+            text: 'fare la cena',
+            done: true
+          },
+          {
+            text: 'fare esercizio',
+            done: false
+          },
+
         ],
       }
     },
     methods: {
         addTask() {
             this.tasks.push(this.newTask)
-           
-        }
+        },
+        removeTask(index) {
+          this.tasks.splice(index, 1);
+        },
     }
   }).mount('#app')
